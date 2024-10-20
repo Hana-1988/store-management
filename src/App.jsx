2c3839a9ac18ea32832f206794e227cffa3e5e56
Hana Pourmohebbi <h.pourmohebbi@gmail.com>
2024-10-20 16:45:55 +0330

    import BrowserRouter as Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import ProductsList from "./components/ProductsList";
-import {Route, Router, Routes } from "react-router-dom";
+import {Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
 function App() {
   const queryClient = new QueryClient();
