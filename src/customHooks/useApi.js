c37d4b825360a02cdf3243f0ec3eae92d9eca68c
Hana Pourmohebbi <h.pourmohebbi@gmail.com>
2024-10-20 16:05:46 +0330

    delete base url from methods

--- src/customHooks/useApi.js
+++ src/customHooks/useApi.js
@@ -1,21 +1,20 @@
-import { useQuery,useMutation,useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
 
 async function fetchProducts(){
-    const {data}= await axios.get("http://localhost:3000/products");
+    const {data}= await axios.get("/products");
     return data;
 }
 
 async function addProduct(newProduct){
-    return axios.post("http://localhost:3000/products",newProduct);
+    return axios.post("/products",newProduct);
 }
 
 async function updateProduct({id,updatedproduct}){
-    return axios.put(`http://localhost:3000/products/${id}`,updatedproduct);
+    return axios.put(`/products/${id}`,updatedproduct);
 }
 
 async function deleteProduct(id){
-    return axios.delete(`http://localhost:3000/products/${id}`);
+    return axios.delete(`/products/${id}`);
 }
 
 export {
