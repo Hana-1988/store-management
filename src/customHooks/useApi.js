50d06a5dce402352b3b685b7b3cf51865e64940b
Hana Pourmohebbi <h.pourmohebbi@gmail.com>
2024-10-21 10:32:57 +0330

    change api connfig

--- src/customHooks/useApi.js
+++ src/customHooks/useApi.js
@@ -1,25 +1,68 @@
-import axios from "axios";
+// import axios from "axios";
 
-async function fetchProducts(){
-    const {data}= await axios.get("/products");
-    return data;
+// async function fetchProducts(){
+//     const {data}= await axios.get("/products");
+//     return data;
+// }
+
+// async function addProduct(newProduct){
+//     return axios.post("/products",newProduct);
+// }
+
+// async function updateProduct({id,updatedproduct}){
+//     return axios.put(`/products/${id}`,updatedproduct);
+// }
+
+// async function deleteProduct(id){
+//     return axios.delete(`/products/${id}`);
+// }
+
+// export {
+//     fetchProducts,
+//     addProduct,
+//     updateProduct,
+//     deleteProduct,
+// };
+
+
+import axios from 'axios';
+
+const api = 'https://localhost:3000/products'; // لینک به Swagger یا Mock API
+
+// Create (Add new product)
+async function addProduct (product) {
+  const response = await axios.post(api, product);
+  return response.data;
+};
+
+// Read (Get all products)
+async function getProducts() {
+  const response = await axios.get(api);
+  return response.data;
 }
 
-async function addProduct(newProduct){
-    return axios.post("/products",newProduct);
+// Update (Edit product)
+async function updateProduct (product) {
+  const response = await axios.put(`${api}/${product.id}`, product);
+  return response.data;
 }
 
-async function updateProduct({id,updatedproduct}){
-    return axios.put(`/products/${id}`,updatedproduct);
+// Delete (Delete product)
+async function deleteProduct (productId){
+  const response = await axios.delete(`${api}/${productId}`);
+  return response.data;
 }
 
-async function deleteProduct(id){
-    return axios.delete(`/products/${id}`);
+// Bulk Delete (Delete multiple products)
+async function deleteProductsBulk (productIds) {
+  const response = await axios.post(`${api}/bulk-delete`, { ids: productIds });
+  return response.data;
 }
 
 export {
-    fetchProducts,
     addProduct,
-    updateProduct,
     deleteProduct,
-};
+    updateProduct,
+    getProducts,
+    deleteProductsBulk,
+}
