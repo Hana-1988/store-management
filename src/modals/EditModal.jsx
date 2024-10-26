39f94b0dc6337d858dccfde086b43e96f3d24d28
Hana Pourmohebbi <h.pourmohebbi@gmail.com>
2024-10-26 21:48:57 +0330

    edit product modal jsx code and ite syles

--- src/modals/EditModal.jsx
+++ src/modals/EditModal.jsx
@@ -1,9 +1,72 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import Modal from 'react-modal';
+import styles from'./EditModal.module.css';
 
-function EditModal() {
+function EditModal ({ isOpen, onRequestClose, initialData, onSubmit }){
+  const [product, setProduct] = useState({
+    name: '',
+    quantity: '',
+    price: ''
+  });
+
+  useEffect(() => {
+    if (initialData) {
+      setProduct(initialData);
+    }
+  }, [initialData]);
+
+  const handleChange = (e) => {
+    setProduct({
+      ...product,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit(product);
+  };
+
   return (
-    <div>EditModal</div>
-  )
-}
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      className={styles.productModal}
+      overlayClassName={styles.productModalOverlay}
+    >
+      <h3>ویرایش اطلاعات</h3>
+      <form onSubmit={handleSubmit}>
+        <label>نام کالا</label>
+        <input
+          name="name"
+          value={product.name}
+          onChange={handleChange}
+          placeholder="نام کالا"
+          className={styles.inputField}
+        />
+        <label>تعداد موجودی</label>
+        <input
+          name="quantity"
+          value={product.quantity}
+          onChange={handleChange}
+          placeholder="تعداد "
+          className={styles.inputField}
+        />
+        <label>قیمت</label>
+        <input
+          name="price"
+          value={product.price}
+          onChange={handleChange}
+          placeholder="قیمت"
+          className={styles.inputField}
+        />
+        <button type="submit" className={styles.submitBtn}>
+          {initialData ? 'ثبت اطلاعات جدید' : 'ایجاد'}
+        </button>
+        <button onClick={onRequestClose} className={styles.cancelBtn}>انصراف</button>
+      </form>
+    </Modal>
+  );
+};
 
-export default EditModal
+export default EditModal;
