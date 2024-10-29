a6693f366f50a87f84ed077c9dcb0018415c2e60
Hana Pourmohebbi <h.pourmohebbi@gmail.com>
2024-10-29 23:38:05 +0330

    add manager photo and give home page styles

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,6 +4,8 @@ import styles from "./Home.module.css"
 function Home() {
   return (
     <div className={styles.container}>
+      <div><h2>Welcome to the product management section</h2></div>
+      <div className={styles.image}><img src='/manager.png'/></div>
         <button><Link to="/products">Store Management</Link></button>
     </div>
   )
